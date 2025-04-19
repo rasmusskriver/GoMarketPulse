60b38e45d0a4e4031919d36a0dfdb28f72e200a2
Batman <rasmus_skriver@yahoo.dk>
2025-04-19 11:57:12 +0200

    Learning..

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example/user/hello
 
 go 1.24.2
+
+require github.com/google/go-cmp v0.7.0
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 
 	"example/user/hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
